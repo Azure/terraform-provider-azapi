f5e14b86c8f8f0848d02721957a28539d3bf8013
Heng Lu <henglu@microsoft.com>
2021-11-12 13:43:28 +0800

    add example for patch resource

---
+++ examples/containerregistry/2020-11-01-preview/ContainerRegistry_Patch/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurermg = {
+      source = "ms-henglu/azurermg"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurermg" {
+  schema_validation_enabled = false
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctest-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acctest"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "Premium"
+  admin_enabled       = false
+}
+
+resource "azurermg_patch_resource" "test" {
+  resource_id = azurerm_container_registry.acr.id
+  api_version = "2020-11-01-preview"
+  body = <<BODY
+{
+  "properties": {
+    "anonymousPullEnabled": true
+  }
+}
+  BODY
+
+}
