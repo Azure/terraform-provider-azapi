17f2a3138975ecd6d0a2a77d077b68b802392283
Heng Lu <henglu@microsoft.com>
2022-02-07 11:24:16 +0800

    Support `auxiliary_tenant_ids` in provider block

--- utils/helper.go
+++ utils/helper.go
@@ -180,3 +180,15 @@ func GetScopeType(id string) types.ScopeType {
 
 	return types.Unknown
 }
+
+func ExpandStringSlice(input []interface{}) *[]string {
+	result := make([]string, 0)
+	for _, item := range input {
+		if item != nil {
+			result = append(result, item.(string))
+		} else {
+			result = append(result, "")
+		}
+	}
+	return &result
+}
