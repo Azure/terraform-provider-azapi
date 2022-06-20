2dc7833afcdacaaad3c4ced0b1c68637e6496914
Heng Lu <henglu@microsoft.com>
2022-06-20 15:51:19 +0800

    azapi provider support `partner_id`, `disable_terraform_partner_id` and `disable_terraform_partner_id`

--- internal/clients/client.go
+++ internal/clients/client.go
@@ -22,11 +22,12 @@ type Client struct {
 }
 
 type Option struct {
-	SubscriptionId           string
-	Cred                     azcore.TokenCredential
-	ApplicationUserAgent     string
-	Features                 features.UserFeatures
-	SkipProviderRegistration bool
+	SubscriptionId              string
+	Cred                        azcore.TokenCredential
+	ApplicationUserAgent        string
+	Features                    features.UserFeatures
+	SkipProviderRegistration    bool
+	DisableCorrelationRequestID bool
 }
 
 // NOTE: it should be possible for this method to become Private once the top level Client's removed
@@ -38,6 +39,13 @@ func (client *Client) Build(ctx context.Context, o *Option) error {
 	azlog.SetListener(func(cls azlog.Event, msg string) {
 		log.Printf("[DEBUG] %s %s: %s\n", time.Now().Format(time.StampMicro), cls, msg)
 	})
+
+	perCallPolicies := make([]policy.Policy, 0)
+	perCallPolicies = append(perCallPolicies, withUserAgent(o.ApplicationUserAgent))
+	if !o.DisableCorrelationRequestID {
+		perCallPolicies = append(perCallPolicies, withCorrelationRequestID(correlationRequestID()))
+	}
+
 	resourceClient, err := NewResourceClient(o.SubscriptionId, o.Cred, &arm.ClientOptions{
 		ClientOptions: policy.ClientOptions{
 			// Disable the default telemetry policy, because it has a length limitation for user agent
@@ -47,10 +55,7 @@ func (client *Client) Build(ctx context.Context, o *Option) error {
 			Logging: policy.LogOptions{
 				IncludeBody: true,
 			},
-			PerCallPolicies: []policy.Policy{
-				withCorrelationRequestID(correlationRequestID()),
-				withUserAgent(o.ApplicationUserAgent),
-			},
+			PerCallPolicies: perCallPolicies,
 		},
 		DisableRPRegistration: o.SkipProviderRegistration,
 	})
