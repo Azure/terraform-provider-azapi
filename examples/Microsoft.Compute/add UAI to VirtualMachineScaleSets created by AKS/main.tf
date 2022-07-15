1e01380fabe55124e397684afb67157d1da02348
Heng Lu <henglu@microsoft.com>
2022-07-15 13:56:20 +0800

    add examples for azapi_operation

---
+++ examples/Microsoft.Compute/add UAI to VirtualMachineScaleSets created by AKS/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azapi" {
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "myResourceGroup"
+  location = "westeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "test" {
+  name                = "mycluster"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  dns_prefix          = "mycluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "myidentity"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+data "azurerm_virtual_machine_scale_set" "test" {
+  name                = "aks-default-34472765-vmss"
+  resource_group_name = "MC_${azurerm_kubernetes_cluster.test.name}_${azurerm_kubernetes_cluster.test.name}_${azurerm_resource_group.test.location}"
+}
+
+resource "azapi_operation" "test" {
+  type        = "Microsoft.Compute/virtualMachineScaleSets@2022-03-01"
+  resource_id = data.azurerm_virtual_machine_scale_set.test.id
+  // leave operation empty or omit this field, to make request towards the resource
+  operation = ""
+  method    = "PATCH"
+  body = jsonencode({
+    identity = {
+      type = "UserAssigned"
+      userAssignedIdentities = {
+        (azurerm_user_assigned_identity.test.id) = {}
+      }
+    }
+  })
+}
