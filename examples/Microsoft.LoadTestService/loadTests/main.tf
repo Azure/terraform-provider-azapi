beb536f8aba81f27bef00a9e3f81c35e3882ae3a
Heng Lu <henglu@microsoft.com>
2022-10-27 15:20:58 +0800

    add example for load test CMK

---
+++ examples/Microsoft.LoadTestService/loadTests/main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "azure/azapi"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azapi" {
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "myResourceGroup"
+  location = "West Europe"
+}
+
+resource "azurerm_user_assigned_identity" "test" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "test" {
+  name                     = "examplekeyvault"
+  location                 = azurerm_resource_group.test.location
+  resource_group_name      = azurerm_resource_group.test.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
+
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+  }
+
+  access_policy {
+    tenant_id = azurerm_user_assigned_identity.test.tenant_id
+    object_id = azurerm_user_assigned_identity.test.principal_id
+    key_permissions = [
+      "Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify"
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_key" "test" {
+  name         = "example-key"
+  key_vault_id = azurerm_key_vault.test.id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+}
+
+
+resource "azapi_resource" "loadTest" {
+  type      = "Microsoft.LoadTestService/loadTests@2022-12-01"
+  name      = "texample"
+  parent_id = azurerm_resource_group.test.id
+
+  location = azurerm_resource_group.test.location
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.test.id]
+  }
+
+  body = jsonencode({
+    properties = {
+      description = "This is new load test"
+      encryption = {
+        identity = {
+          resourceId = azurerm_user_assigned_identity.test.id
+          type       = "UserAssigned"
+        }
+        keyUrl = azurerm_key_vault_key.example.versionless_id
+      }
+    }
+  })
+
+  tags = {
+    Team = "Dev Exp"
+  }
+}
