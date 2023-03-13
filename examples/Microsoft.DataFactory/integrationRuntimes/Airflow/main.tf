ae02bd31e9d7f1dc1b8814646c8509022bed5410
Bram <10193236+bramvdklinkenberg@users.noreply.github.com>
2023-03-13 03:01:17 +0100

    Add ADF IR Airflow example (#266)
    * Add Azure Data Factory - Managed Airflow example
    
    * Add comment with regards to feature being in public preview
    
    * Update main.tf
    
    ---------
    
    Co-authored-by: Heng Lu <79895375+ms-henglu@users.noreply.github.com>

---
+++ examples/Microsoft.DataFactory/integrationRuntimes/Airflow/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azapi" {
+}
+
+resource "azurerm_resource_group" "example_rg" {
+  name     = "henglu-resource-group"
+  location = "West Europe"
+}
+
+resource "azurerm_data_factory" "example_adf" {
+  name                = "henglu"
+  location            = azurerm_resource_group.example_rg.location
+  resource_group_name = azurerm_resource_group.example_rg.name
+}
+
+# This feature is in public preview at time of writing, hence the `schema_validation_enabled = false`
+resource "azapi_resource" "example_ir" {
+  type                      = "Microsoft.DataFactory/factories/integrationRuntimes@2018-06-01"
+  name                      = "henglu"
+  parent_id                 = azurerm_data_factory.example_adf.id
+  schema_validation_enabled = false
+
+  body = jsonencode({
+    properties = {
+      type = "Airflow"
+      typeProperties = {
+        computeProperties = {
+          location    = "West Europe"
+          computeSize = "Small"
+          extraNodes  = 0
+        }
+        airflowProperties = {
+          version                  = "2.2.2"
+          enableAADIntegration     = true
+          airflowRequiredArguments = ["airflow.providers.microsoft.azure"]
+        }
+      }
+    }
+  })
+}
