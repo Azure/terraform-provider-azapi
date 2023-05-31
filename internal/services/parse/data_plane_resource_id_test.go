5d251c27447861978f5c49a33ad964271ecc93eb
Heng Lu <henglu@microsoft.com>
2023-05-31 15:55:12 +0800

    fix unit test

--- internal/services/parse/data_plane_resource_id_test.go
+++ internal/services/parse/data_plane_resource_id_test.go
@@ -1,6 +1,10 @@
-package parse
+package parse_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Azure/terraform-provider-azapi/internal/services/parse"
+)
 
 func Test_NewDataPlaneResourceId(t *testing.T) {
 	testData := []struct {
@@ -8,14 +12,14 @@ func Test_NewDataPlaneResourceId(t *testing.T) {
 		ParentId     string
 		ResourceType string
 		Error        bool
-		Expected     *DataPlaneResourceId
+		Expected     *parse.DataPlaneResourceId
 	}{
 		{
 			Name:         "test",
 			ParentId:     "xxx.xxx.xxx",
 			ResourceType: "Microsoft.AppConfiguration/configurationStores/keyValues@api-version",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "xxx.xxx.xxx/kv/test",
 				ApiVersion:        "api-version",
 				AzureResourceType: "Microsoft.AppConfiguration/configurationStores/keyValues",
@@ -26,7 +30,7 @@ func Test_NewDataPlaneResourceId(t *testing.T) {
 			ParentId:     "xxx.xxx.xxx",
 			ResourceType: "Microsoft.DeviceUpdate/accounts/v2/groups@8.2",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "xxx.xxx.xxx/v2/management/groups/test",
 				ApiVersion:        "8.2",
 				AzureResourceType: "Microsoft.DeviceUpdate/accounts/v2/groups",
@@ -37,7 +41,7 @@ func Test_NewDataPlaneResourceId(t *testing.T) {
 			ParentId:     "xxx.xxx.xxx",
 			ResourceType: "Microsoft.IoTCentral/iotApps/devices/attestation@8.2",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "xxx.xxx.xxx/api/devices/test/attestation",
 				ApiVersion:        "8.2",
 				AzureResourceType: "Microsoft.IoTCentral/iotApps/devices/attestation",
@@ -48,7 +52,7 @@ func Test_NewDataPlaneResourceId(t *testing.T) {
 			ParentId:     "foo.keyvault.azure.net",
 			ResourceType: "Microsoft.KeyVault/vaults/certificates/contacts@v1.1-preview.2",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "foo.keyvault.azure.net/certificates/contacts",
 				ApiVersion:        "v1.1-preview.2",
 				AzureResourceType: "Microsoft.KeyVault/vaults/certificates/contacts",
@@ -59,7 +63,7 @@ func Test_NewDataPlaneResourceId(t *testing.T) {
 	for _, v := range testData {
 		t.Logf("[DEBUG] Testing %q %q %q", v.Name, v.ParentId, v.ResourceType)
 
-		actual, err := NewDataPlaneResourceId(v.Name, v.ParentId, v.ResourceType)
+		actual, err := parse.NewDataPlaneResourceId(v.Name, v.ParentId, v.ResourceType)
 		if err != nil {
 			if v.Error {
 				continue
@@ -88,13 +92,13 @@ func Test_DataPlaneResourceIDWithResourceType(t *testing.T) {
 		ResourceId   string
 		ResourceType string
 		Error        bool
-		Expected     *DataPlaneResourceId
+		Expected     *parse.DataPlaneResourceId
 	}{
 		{
 			ResourceId:   "xxx.xxx.xxx/kv/test",
 			ResourceType: "Microsoft.AppConfiguration/configurationStores/keyValues@api-version",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "xxx.xxx.xxx/kv/test",
 				ApiVersion:        "api-version",
 				AzureResourceType: "Microsoft.AppConfiguration/configurationStores/keyValues",
@@ -106,7 +110,7 @@ func Test_DataPlaneResourceIDWithResourceType(t *testing.T) {
 			ResourceId:   "xxx.xxx.xxx/v2/management/groups/test",
 			ResourceType: "Microsoft.DeviceUpdate/accounts/v2/groups@8.2",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "xxx.xxx.xxx/v2/management/groups/test",
 				ApiVersion:        "8.2",
 				AzureResourceType: "Microsoft.DeviceUpdate/accounts/v2/groups",
@@ -118,7 +122,7 @@ func Test_DataPlaneResourceIDWithResourceType(t *testing.T) {
 			ResourceId:   "xxx.xxx.xxx/api/devices/test/attestation",
 			ResourceType: "Microsoft.IoTCentral/iotApps/devices/attestation@8.2",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "xxx.xxx.xxx/api/devices/test/attestation",
 				ApiVersion:        "8.2",
 				AzureResourceType: "Microsoft.IoTCentral/iotApps/devices/attestation",
@@ -130,7 +134,7 @@ func Test_DataPlaneResourceIDWithResourceType(t *testing.T) {
 			ResourceId:   "foo.keyvault.azure.net/certificates/contacts",
 			ResourceType: "Microsoft.KeyVault/vaults/certificates/contacts@v1.1-preview.2",
 			Error:        false,
-			Expected: &DataPlaneResourceId{
+			Expected: &parse.DataPlaneResourceId{
 				AzureResourceId:   "foo.keyvault.azure.net/certificates/contacts",
 				ApiVersion:        "v1.1-preview.2",
 				AzureResourceType: "Microsoft.KeyVault/vaults/certificates/contacts",
@@ -143,7 +147,7 @@ func Test_DataPlaneResourceIDWithResourceType(t *testing.T) {
 	for _, v := range testData {
 		t.Logf("[DEBUG] Testing %q %q", v.ResourceId, v.ResourceType)
 
-		actual, err := DataPlaneResourceIDWithResourceType(v.ResourceId, v.ResourceType)
+		actual, err := parse.DataPlaneResourceIDWithResourceType(v.ResourceId, v.ResourceType)
 		if err != nil {
 			if v.Error {
 				continue
