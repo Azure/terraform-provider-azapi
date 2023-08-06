236d6b14b126fe378831a836a605192e955dfe1d
Arindam Mitra <mail2arindam2003@yahoo.com>
2023-08-06 14:55:44 +0200

    Update main.tf

--- examples/Microsoft.Dashboard/grafana/main.tf
+++ examples/Microsoft.Dashboard/grafana/main.tf
@@ -29,3 +29,39 @@ provider "azurerm" {
 provider "azapi" {
 }
 
+#####################
+## Resource Group:-
+#####################
+
+resource "azurerm_resource_group" "azrg" {
+  name        = var.rg-name
+  location    = var.rg-location
+}
+
+##############################
+## Azure Managed Grafana:-
+##############################
+resource "azapi_resource" "azgrafana" {
+  type        = "Microsoft.Dashboard/grafana@2022-08-01" 
+  name        = var.az-grafana-name
+  parent_id   = azurerm_resource_group.azrg.id
+  location    = azurerm_resource_group.azrg.location
+  
+  identity {
+    type      = "SystemAssigned"
+  }
+
+  body = jsonencode({
+    sku = {
+      name = "Standard"
+    }
+    properties = {
+      publicNetworkAccess = "Enabled",
+      zoneRedundancy = "Enabled",
+      apiKey = "Enabled",
+      deterministicOutboundIP = "Enabled"
+    }
+  })
+
+}
+
