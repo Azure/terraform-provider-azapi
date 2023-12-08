54ce73efb8e41b5e00876a945426c6b94f506505
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-12-08 13:02:06 +0000

    chore: uncomment var checks

--- internal/acceptance/testcase.go
+++ internal/acceptance/testcase.go
@@ -123,13 +123,13 @@ func (td TestData) externalProviders() map[string]resource.ExternalProvider {
 
 func PreCheck(t *testing.T) {
 	variables := []string{
-		// "ARM_CLIENT_ID",
-		// "ARM_CLIENT_SECRET",
-		// "ARM_SUBSCRIPTION_ID",
-		// "ARM_TENANT_ID",
-		// "ARM_TEST_LOCATION",
-		// "ARM_TEST_LOCATION_ALT",
-		// "ARM_TEST_LOCATION_ALT2",
+		"ARM_CLIENT_ID",
+		"ARM_CLIENT_SECRET",
+		"ARM_SUBSCRIPTION_ID",
+		"ARM_TENANT_ID",
+		"ARM_TEST_LOCATION",
+		"ARM_TEST_LOCATION_ALT",
+		"ARM_TEST_LOCATION_ALT2",
 	}
 
 	for _, variable := range variables {
