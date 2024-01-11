dc3c0cab3fd6f5dc8eb3fffb8fd19baf223d9276
Heng Lu <79895375+ms-henglu@users.noreply.github.com>
2024-01-11 15:27:42 +0800

    deprecate the `default_naming_prefix`, `default_naming_suffix` and `removing_special_chars` (#397)

--- internal/azure/resourceName/resourceName.go
+++ internal/azure/resourceName/resourceName.go
@@ -23,6 +23,7 @@ func SchemaResourceNamePrefix() *schema.Schema {
 	return &schema.Schema{
 		Type:          schema.TypeString,
 		Optional:      true,
+		Deprecated:    "It will not work in the next minor release and will be removed in the next major release. Please specify the naming prefix and suffix in the resource's `name` field instead.",
 		ConflictsWith: []string{"default_name"},
 	}
 }
@@ -31,14 +32,16 @@ func SchemaResourceNameSuffix() *schema.Schema {
 	return &schema.Schema{
 		Type:          schema.TypeString,
 		Optional:      true,
+		Deprecated:    "It will not work in the next minor release and will be removed in the next major release. Please specify the naming prefix and suffix in the resource's `name` field instead.",
 		ConflictsWith: []string{"default_name"},
 	}
 }
 
 func SchemaResourceNameRemovingSpecialCharacters() *schema.Schema {
 	return &schema.Schema{
-		Type:     schema.TypeBool,
-		Optional: true,
-		Default:  false,
+		Type:       schema.TypeBool,
+		Optional:   true,
+		Deprecated: "It will not work in the next minor release and will be removed in the next major release. Please specify the `name` field and remove the special characters in the `name` field instead.",
+		Default:    false,
 	}
 }
