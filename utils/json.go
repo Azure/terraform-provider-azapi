6031bffbd7e61a0e9dde690080b46f1fde169955
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-07-09 10:06:39 +0100

    fix: implement suggestion by @mcleanbc and update tests

--- utils/json.go
+++ utils/json.go
@@ -106,7 +106,7 @@ func UpdateObject(old interface{}, new interface{}, option UpdateJsonOption) int
 
 			for _, oldItem := range oldValue {
 				for index, newItem := range newArr {
-					if areSameArrayItems(oldItem, newItem) {
+					if reflect.DeepEqual(oldItem, newItem) && !used[index] {
 						res = append(res, UpdateObject(oldItem, newItem, option))
 						used[index] = true
 						break
