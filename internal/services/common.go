bed057e5c1c75b7d6da306eb2a909b3d750e30bc
Harry Qu <harryqu@microsoft.com>
2024-08-26 18:07:13 +1000

    Update common.go

--- internal/services/common.go
+++ internal/services/common.go
@@ -4,15 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
-
 	"github.com/Azure/terraform-provider-azapi/internal/docstrings"
 	"github.com/Azure/terraform-provider-azapi/internal/services/dynamic"
 	"github.com/Azure/terraform-provider-azapi/internal/services/myplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 func CommonAttributeResponseExportValues() schema.DynamicAttribute {
@@ -26,8 +23,12 @@ func CommonAttributeResponseExportValues() schema.DynamicAttribute {
 }
 
 func buildOutputFromBody(ctx context.Context, responseBody interface{}, modelResponseExportValues types.Dynamic) (types.Dynamic, error) {
-	if modelResponseExportValues.IsNull() || modelResponseExportValues.IsUnknown() {
+	if modelResponseExportValues.IsNull() {
 		return types.DynamicNull(), nil
+	}
+
+	if modelResponseExportValues.IsUnknown() {
+		return types.DynamicUnknown(), nil
 	}
 
 	data, err := dynamic.ToJSON(modelResponseExportValues)
@@ -35,30 +36,19 @@ func buildOutputFromBody(ctx context.Context, responseBody interface{}, modelRes
 		return types.DynamicNull(), err
 	}
 
-	tflog.Debug(ctx, fmt.Sprintf("data: %+v\n", data))
-	tflog.Debug(ctx, fmt.Sprintf("modelResponseExportValues is: %+v\n", modelResponseExportValues))
-	tflog.Debug(ctx, fmt.Sprintf("modelResponseExportValues underlying value is: %+v\n", modelResponseExportValues.UnderlyingValue()))
-	tflog.Debug(ctx, fmt.Sprintf("modelResponseExportValues underlying value type is: %+v\n", modelResponseExportValues.UnderlyingValue().Type(ctx)))
 	switch modelResponseExportValues.UnderlyingValue().(type) {
 	case types.List, types.Tuple, types.Set:
-		tflog.Debug(ctx, fmt.Sprintf("it is a list\n"))
 		var responseExportValues []string
 		if err = json.Unmarshal(data, &responseExportValues); err != nil {
-			tflog.Debug(ctx, fmt.Sprintf("list error: %+v\n", err))
 			return types.DynamicNull(), err
 		}
 
-		tflog.Debug(ctx, fmt.Sprintf("responseExportValues is: %+v\n", responseExportValues))
 		return types.DynamicValue(flattenOutput(responseBody, responseExportValues)), nil
 	case types.Map, types.Object:
-		tflog.Debug(ctx, fmt.Sprintf("it is a map\n"))
 		var responseExportValues map[string]string
 		if err = json.Unmarshal(data, &responseExportValues); err != nil {
-			fmt.Printf("map error: %+v\n", err)
 			return types.DynamicNull(), err
 		}
-
-		tflog.Debug(ctx, fmt.Sprintf("responseExportValues is: %+v\n", responseExportValues))
 
 		return types.DynamicValue(flattenOutputJMES(responseBody, responseExportValues)), nil
 	default:
