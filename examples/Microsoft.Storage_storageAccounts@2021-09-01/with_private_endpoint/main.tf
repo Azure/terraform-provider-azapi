c7338ade7cf42cfa29e9654f4c63760c96c2d50a
Heng Lu <henglu@microsoft.com>
2024-10-15 14:25:59 +0800

    add example for storage account with private endpoint

---
+++ examples/Microsoft.Storage_storageAccounts@2021-09-01/with_private_endpoint/main.tf
@@ -0,0 +1,289 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = var.subscription_id
+}
+
+provider "azapi" {
+  skip_provider_registration = false
+}
+
+variable "resource_name" {
+  type    = string
+  default = "acctest0001"
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "subscription_id" {
+  type    = string
+  default = "00000000-0000-0000-0000-000000000000"
+}
+
+variable "vm_admin_username" {
+  type    = string
+  default = "adminuser"
+}
+
+variable "vm_admin_password" {
+  type      = string
+  default   = "P@$$w0rd1234!"
+  sensitive = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.resource_name}-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "main"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "vm" {
+  name                 = "vm"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone" "queue" {
+  name                = "privatelink.queue.core.windows.net"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone" "web" {
+  name                = "privatelink.web.core.windows.net"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "web" {
+  name                  = "web"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.web.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
+  name                  = "blob"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "queue" {
+  name                  = "queue"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.queue.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "${var.resource_name}-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "example" {
+  name                = "${var.resource_name}-bastion"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Basic"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "${var.resource_name}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = var.resource_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = var.vm_admin_username
+  admin_password      = var.vm_admin_password
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azapi_resource" "storageAccount" {
+  type      = "Microsoft.Storage/storageAccounts@2023-05-01"
+  parent_id = azurerm_resource_group.example.id
+  name      = "${var.resource_name}sa"
+  location  = azurerm_resource_group.example.location
+  body = {
+    kind = "StorageV2"
+    properties = {
+      accessTier                   = "Hot"
+      allowBlobPublicAccess        = false
+      allowCrossTenantReplication  = false
+      allowSharedKeyAccess         = true
+      defaultToOAuthAuthentication = false
+      dnsEndpointType              = "Standard"
+      encryption = {
+        keySource = "Microsoft.Storage"
+        services = {
+          blob = {
+            enabled = true
+            keyType = "Account"
+          }
+          file = {
+            enabled = true
+            keyType = "Account"
+          }
+        }
+      }
+      isHnsEnabled       = false
+      isLocalUserEnabled = true
+      isNfsV3Enabled     = false
+      isSftpEnabled      = false
+      minimumTlsVersion  = "TLS1_2"
+      networkAcls = {
+        bypass              = "AzureServices"
+        defaultAction       = "Allow"
+        ipRules             = []
+        resourceAccessRules = []
+        virtualNetworkRules = []
+      }
+      publicNetworkAccess      = "Disabled"
+      supportsHttpsTrafficOnly = true
+    }
+    sku = {
+      name = "Standard_LRS"
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = azapi_resource.storageAccount.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_windows_virtual_machine.example.identity[0].principal_id
+}
+
+resource "azurerm_private_endpoint" "blob" {
+  name                = "${var.resource_name}-private-endpoint-blob"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_service_connection {
+    name                           = "${var.resource_name}-private-endpoint-connection"
+    private_connection_resource_id = azapi_resource.storageAccount.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "storage-private-endpoint-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.blob.id]
+  }
+}
+
+resource "azurerm_private_endpoint" "queue" {
+  name                = "${var.resource_name}-private-endpoint-queue"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_service_connection {
+    name                           = "${var.resource_name}-private-endpoint-connection-q"
+    private_connection_resource_id = azapi_resource.storageAccount.id
+    subresource_names              = ["queue"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "storage-private-endpoint-dns-zone-group-q"
+    private_dns_zone_ids = [azurerm_private_dns_zone.queue.id]
+  }
+}
+
+resource "azurerm_private_endpoint" "web" {
+  name                = "${var.resource_name}-private-endpoint-web"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_service_connection {
+    name                           = "${var.resource_name}-private-endpoint-connection-web"
+    private_connection_resource_id = azapi_resource.storageAccount.id
+    subresource_names              = ["web"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "storage-private-endpoint-dns-zone-group-web"
+    private_dns_zone_ids = [azurerm_private_dns_zone.web.id]
+  }
+}
+
