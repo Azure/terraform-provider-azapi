82c06b5bee78f86b414bd3ec159612205d9b7664
neil-yechenwei <yechenwei2007@hotmail.com>
2025-02-18 14:00:34 +0800

    Add example for Microsoft.AppPlatform/Spring/buildServices

---
+++ examples/Microsoft.Web_staticSites_config@2022-03-01/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+provider "azapi" {
+  skip_provider_registration = false
+}
+
+variable "resource_name" {
+  type    = string
+  default = "acctest0001"
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+resource "azapi_resource" "resourceGroup" {
+  type     = "Microsoft.Resources/resourceGroups@2020-06-01"
+  name     = var.resource_name
+  location = var.location
+}
+
+resource "azapi_resource" "staticSite" {
+  type      = "Microsoft.Web/staticSites@2021-02-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
+  body = {
+    properties = {
+    }
+    sku = {
+      name = "Free"
+      tier = "Free"
+    }
+  }
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+}
+
+resource "azapi_resource" "config" {
+  type      = "Microsoft.Web/staticSites/config@2022-03-01"
+  name      = "appsettings"
+  parent_id = azapi_resource.staticSite.id
+
+  body = {
+    properties = {
+      password                   = "Password123!@"
+      secretState                = "Password"
+      applicableEnvironmentsMode = "AllEnvironments"
+    }
+  }
+}
