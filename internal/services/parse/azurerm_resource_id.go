dca7b3041e91e21dbb1e19fe3e8d1adc5c3af9e2
Heng Lu <henglu@microsoft.com>
2025-02-28 14:40:26 +0800

    bugfix: format azurerm resource ID when move resource

---
+++ internal/services/parse/azurerm_resource_id.go
@@ -0,0 +1,23 @@
+package parse
+
+import (
+	"fmt"
+	"strings"
+)
+
+// AzurermIdToAzureId converts an azurerm id to an azure resource id
+func AzurermIdToAzureId(azurermResourceType string, azurermId string) (string, error) {
+	switch azurermResourceType {
+	case "azurerm_monitor_diagnostic_setting":
+		// input: <target id>|<diagnostic setting name>
+		// output: <target id>/providers/Microsoft.Insights/diagnosticSettings/<diagnostic setting name>
+		azurermIdSplit := strings.Split(azurermId, "|")
+		if len(azurermIdSplit) != 2 {
+			return "", fmt.Errorf("invalid id: %s, expected format: <target id>|<diagnostic setting name>", azurermId)
+		}
+		return fmt.Sprintf("%s/providers/Microsoft.Insights/diagnosticSettings/%s", azurermIdSplit[0], azurermIdSplit[1]), nil
+		// add more cases here as needed
+	}
+	// return azure id
+	return azurermId, nil
+}
---
+++ internal/services/parse/azurerm_resource_id_test.go
@@ -0,0 +1,45 @@
+package parse_test
+
+import (
+	"testing"
+
+	"github.com/Azure/terraform-provider-azapi/internal/services/parse"
+)
+
+func Test_AzurermIdToAzureId(t *testing.T) {
+	testcases := []struct {
+		name                string
+		azurermResourceType string
+		azurermId           string
+		expectedAzureId     string
+		expectError         bool
+	}{
+		{
+			name:                "azurerm_monitor_diagnostic_setting valid",
+			azurermResourceType: "azurerm_monitor_diagnostic_setting",
+			azurermId:           "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Compute/virtualMachines/vm|diagnosticSettingName",
+			expectedAzureId:     "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Compute/virtualMachines/vm/providers/Microsoft.Insights/diagnosticSettings/diagnosticSettingName",
+			expectError:         false,
+		},
+		{
+			name:                "azurerm_monitor_diagnostic_setting invalid",
+			azurermResourceType: "azurerm_monitor_diagnostic_setting",
+			azurermId:           "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Compute/virtualMachines/vm|diagnosticSettingName|foo",
+			expectedAzureId:     "",
+			expectError:         true,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			azureId, err := parse.AzurermIdToAzureId(tc.azurermResourceType, tc.azurermId)
+			if (err != nil) != tc.expectError {
+				t.Fatalf("expected error: %v, got: %v", tc.expectError, err)
+			}
+			if azureId != tc.expectedAzureId {
+				t.Fatalf("expected azure id: %s, got: %s", tc.expectedAzureId, azureId)
+			}
+		})
+	}
+
+}
