b26b8772d1d26b839918dc0fcac5eb91d276589d
Heng Lu <henglu@microsoft.com>
2025-09-29 14:55:50 +0800

    fix tests

--- internal/services/azapi_resource_test.go
+++ internal/services/azapi_resource_test.go
@@ -58,7 +58,7 @@ func TestAccGenericResource_resourceGroup(t *testing.T) {
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
 		},
-		data.ImportStep(defaultIgnores()...),
+		data.ImportStepWithImportStateIdFunc(r.ImportIdFunc, defaultIgnores()...),
 	})
 }
 
