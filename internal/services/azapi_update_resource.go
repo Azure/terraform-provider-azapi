2e78b19ee6a1c2303d0be92188644c5110a67230
magodo <wztwcy@gmail.com>
2026-03-31 17:40:26 +1100

    Migrate azapi_update_resource resource

--- internal/services/azapi_update_resource.go
+++ internal/services/azapi_update_resource.go
@@ -34,6 +34,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+	tffwdocs "github.com/magodo/terraform-plugin-framework-docs"
 )
 
 type AzapiUpdateResourceModel struct {
@@ -70,6 +71,7 @@ var _ resource.ResourceWithConfigure = &AzapiUpdateResource{}
 var _ resource.ResourceWithValidateConfig = &AzapiUpdateResource{}
 var _ resource.ResourceWithModifyPlan = &AzapiUpdateResource{}
 var _ resource.ResourceWithUpgradeState = &AzapiUpdateResource{}
+var _ tffwdocs.ResourceWithRenderOption = &AzapiUpdateResource{}
 
 func (r *AzapiUpdateResource) Configure(ctx context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
 	if v, ok := request.ProviderData.(*clients.Client); ok {
@@ -668,4 +670,83 @@ func (r *AzapiUpdateResource) Read(ctx context.Context, request resource.ReadReq
 
 func (r *AzapiUpdateResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
 
+}
+
+func (r *AzapiUpdateResource) RenderOption() tffwdocs.ResourceRenderOption {
+	return tffwdocs.ResourceRenderOption{
+		Examples: []tffwdocs.Example{
+			{
+				HCL: `
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+provider "azapi" {
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "west europe"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "example" {
+  name                = "example-lb"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_nat_rule" "example" {
+  resource_group_name            = azurerm_resource_group.example.name
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "RDPAccess"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azapi_update_resource" "example" {
+  type        = "Microsoft.Network/loadBalancers@2021-03-01"
+  resource_id = azurerm_lb.example.id
+
+  body = {
+    properties = {
+      inboundNatRules = [
+        {
+          properties = {
+            idleTimeoutInMinutes = 15
+          }
+        }
+      ]
+    }
+  }
+
+  depends_on = [
+    azurerm_lb_nat_rule.example,
+  ]
+}
+`,
+			},
+		},
+	}
 }
