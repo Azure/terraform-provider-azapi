6d59141639a8a3b26cd927339cecbca9066679ac
Gerry Tan <2316198+gerrytan@users.noreply.github.com>
2026-04-28 16:43:23 +1000

    Change acctest VM size from Standard_DS2_v2 to Standard_D2s_v3, it has wider location support

--- internal/services/azapi_resource_preflight_test.go
+++ internal/services/azapi_resource_preflight_test.go
@@ -269,7 +269,7 @@ resource "azapi_resource" "aksCluster" {
           count  = 1
           mode   = "System"
           name   = "default"
-          vmSize = "Standard_DS2_v2"
+          vmSize = "Standard_D2s_v3"
         },
       ]
       dnsPrefix = "exampleaks"
